6691a90e00d0603676d8b9dd33f8e6c40c33c431
Crypto Korin <cryptokorin@gmail.com>
2022-01-23 11:58:27 +0100

    add description text area

--- src/Description.js
+++ src/Description.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   FormControl,
   FormLabel,
@@ -5,48 +6,52 @@ import {
   Button,
   Flex,
   useClipboard,
+  Textarea,
 } from '@chakra-ui/react';
 
 const Description = ({title, punycode , nmcAsset, registration}) => {
   const { hasCopied, onCopy } = useClipboard(title);
+  const [description, setDescription] = useState("");
+  const { hasCopied: hasCopiedDescription, onCopy: onCopyDescription } = useClipboard(description);
+  
+  useEffect(() => {
+    if (punycode) {
+      setDescription(`Decoded Asset: ${punycode}
+Asset: ${nmcAsset}
+Mint: ${registration}
+      
+A few pioneers in the blockchain space used an encoding language called "punycode" to encode various forms of art, emojis, alphabets and words as non-fungible assets,onto the Namecoin blockchain. In hindsight, this gave birth to one of the 1st NFT collectionsin the history of cryptographic collectables: Punycodes.
+      
+Note, Namecoin is a DNS & thus assets need to be renewed (every 9 months). Have a small $NMC in your vault & it'll autorenew.
+1 $NMC = Autorenewal for 5 years
+10 $NMC = Autorenewal for 50 years`)
+    }
+  }, [nmcAsset, punycode, registration])
 
    return (
     <div>
-        <FormControl w="50%">
+        <FormControl w="50%" mb={4} mt="17%">
           <FormLabel>Title</FormLabel>
-          <Flex >
+          <Flex>
             <Input type="text" value={title} isReadOnly={true} />
             <Button onClick={onCopy} ml={2}>
               {hasCopied ? 'Copied' : 'Copy'}
             </Button>
           </Flex>
         </FormControl>
-        <ul>
-          <li>
-            Decoded Asset: {punycode}
-          </li>
-          <li>
-            Asset: {nmcAsset}
-          </li>
-          <li>
-            Mint: {registration}
-          </li>
-        </ul>
-        <p>
-        A few pioneers in the blockchain space used an encoding language called "punycode" 
-        to encode various forms of art, emojis, alphabets and words as non-fungible assets, 
-        onto the Namecoin blockchain. In hindsight, this gave birth to one of the 1st NFT 
-        collections in the history of cryptographic collectables: Punycodes.
-        </p>
-        <p>
-        Note, Namecoin is a DNS & thus assets need to be renewed (every 9 months). Have a small $NMC in your vault & it'll autorenew.
-        </p>
-        <p>
-        1 $NMC = Autorenewal for 5 years 
-        </p>
-        <p>
-        10 $NMC = Autorenewal for 50 years
-        </p>
+        <FormControl w="80%">
+          <FormLabel>Description</FormLabel>
+          <Flex >
+          <Textarea
+              value={description}
+              isReadOnly
+              h="40vh"
+          />
+            <Button onClick={onCopyDescription} ml={2}>
+              {hasCopiedDescription ? 'Copied' : 'Copy'}
+            </Button>
+          </Flex>
+        </FormControl>
     </div>
    )
 }
